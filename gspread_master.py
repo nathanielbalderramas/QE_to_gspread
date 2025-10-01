b7eeed8723027ceaae5af80c20e362a7a92c0ffb
Nathaniel Balderramas <83505517+nathanielbalderramas@users.noreply.github.com>
2025-10-01 10:59:37 -0300

    Update gspread_master.py

--- gspread_master.py
+++ gspread_master.py
@@ -2,7 +2,7 @@ import pandas as pd
 from gspread_pandas import Spread, Client
 
 from report_master import generate_reports
-from gspread_handler import update_raw, update_dash
+from gspread_handler import update_raw, update_pivots
 
 from pivot_functions import (CAT_final_energy, 
                              N2_adsorption_energy, 
@@ -42,17 +42,17 @@ if __name__ == "__main__":
 
 
     # Subimos lo que querramos
-    update_raw()
-    dash_pivots =[[CAT_final_energy, "Final Energy [CAT] (eV)", "A5", "DASH"],
-                 [N2_adsorption_energy, "N2 Eads (eV)", "A20", "DASH"],
-                 [N2_disociative_adsorption_energy, "N2 Eads+dis (eV)", "A35", "DASH"],
-                 [N2_surface_disociation_energy, "N2 Esurfdis", "A50", "DASH"],
-                ]
-    update_pivots(dash_pivots)
+    update_raw(df, spread)
+    #dash_pivots =[[CAT_final_energy, "Final Energy [CAT] (eV)", "A5", "DASH"],
+    #             [N2_adsorption_energy, "N2 Eads (eV)", "A20", "DASH"],
+    #             [N2_disociative_adsorption_energy, "N2 Eads+dis (eV)", "A35", "DASH"],
+    #             [N2_surface_disociation_energy, "N2 Esurfdis", "A50", "DASH"],
+    #            ]
+    #update_pivots(df, spread, dash_pivots)
 
     stat_pivots =[[CAT_status, "CAT status", "A5", "STATUS"],
                  [N2_status, "N2 status", "A20", "STATUS"],
                  [twoN_status, "2N status", "A35", "STATUS"],
                 ]
-    update_pivots(stat_pivots)
+    update_pivots(df, spread, stat_pivots)
 
